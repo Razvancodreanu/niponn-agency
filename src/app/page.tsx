af67d4b24a0b9fe57db87d7aa27cda743840aba6
Razvan Codreanu <razvan.codreanu90@gmail.com>
2025-08-21 15:10:50 +0300

    Homepage simplu și funcțional

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Bine ai venit la Niponn General Construct!</h1>
-      <p>Site-ul este live și funcționează corect.</p>
-    </main>
-  );
-}
